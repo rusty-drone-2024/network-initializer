0878878ee9dde1b8e1b66e46016c9622032132e5
Matteo Di Noia <matteodinoia02@gmail.com>
2024-12-31 12:18:31 +0100

    add test for each drone

--- src/drone_test/mod.rs
+++ src/drone_test/mod.rs
@@ -1,1 +1,17 @@
-// TODO add test of the drones we bought
+#![cfg(test)]
+use std::time::Duration;
+
+mod bagel_bomber;
+mod cpp_enjoyers;
+mod d_r_o_n_e;
+mod dr_ones;
+mod fungi;
+mod le_drones;
+mod lockheed;
+mod rust;
+mod rustafarian;
+mod rustbusters;
+
+const TIMEOUT: Duration = Duration::from_millis(50);
+const FLOOD_TIMEOUT: Duration = Duration::from_millis(100);
+const HARD_TEST_TIMEOUT: Duration = Duration::from_millis(2000);
