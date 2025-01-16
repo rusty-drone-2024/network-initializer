83054cddcf0eb6e1aa517e7e3e42b08707e6a0cf
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-16 16:16:03 +0100

    clearer errors

--- src/network_initializer/parser.rs
+++ src/network_initializer/parser.rs
@@ -2,6 +2,8 @@ use std::fs;
 use wg_2024::config::Config;
 
 pub fn load_from_file(path: &str) -> Config {
-    let config_str = fs::read_to_string(path).expect("Unable to read config file");
-    toml::from_str(&config_str).expect("Unable to parse TOML")
+    let config_str = fs::read_to_string(path)
+        .expect("ERROR[Network Initializer]: is unable to read config file 'config.toml' (please provide one in the root folder)");
+
+    toml::from_str(&config_str).expect("ERROR[Network Initializer]: Unable to parse config.toml")
 }
