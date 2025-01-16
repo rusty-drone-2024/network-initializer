00f942cdddc662ec145b42b5686e5e52af3c82df
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-16 16:48:53 +0100

    show path to which is relative

--- src/network_initializer/parser.rs
+++ src/network_initializer/parser.rs
@@ -1,9 +1,13 @@
 use std::fs;
+use std::path::Path;
 use wg_2024::config::Config;
 
 pub fn load_from_file(path: &str) -> Config {
-    let config_str = fs::read_to_string(path)
-        .expect("ERROR[Network Initializer]: is unable to read config file 'config.toml' (please provide one in the root folder)");
+    let config_str = fs::read_to_string(path).expect(&format!(
+        "ERROR[Network Initializer]: is unable to read config from '{}' relative from path = {:?}",
+        path,
+        Path::new(".").canonicalize()
+    ));
 
     toml::from_str(&config_str).expect("ERROR[Network Initializer]: Unable to parse config.toml")
 }
