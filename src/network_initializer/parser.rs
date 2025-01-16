ffbeac3544658c82494f32d5482e7e134ef6127c
Matteo Di Noia <matteodinoia02@gmail.com>
2025-01-16 16:55:16 +0100

    remove uneeded error info

--- src/network_initializer/parser.rs
+++ src/network_initializer/parser.rs
@@ -1,12 +1,10 @@
 use std::fs;
-use std::path::Path;
 use wg_2024::config::Config;
 
 pub fn load_from_file(path: &str) -> Config {
-    let config_str = fs::read_to_string(path).expect(&format!(
-        "ERROR[Network Initializer]: is unable to read config from '{}' relative from path = {:?}",
+    let config_str = fs::read_to_string(path).unwrap_or_else(|_| panic!(
+        "ERROR[Network Initializer]: is unable to read config from '{}' relative from the root of the project",
         path,
-        Path::new(".").canonicalize()
     ));
 
     toml::from_str(&config_str).expect("ERROR[Network Initializer]: Unable to parse config.toml")
