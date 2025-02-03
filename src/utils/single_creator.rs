0e3239b782221fe1cc16b5613eebd1afb9198109
MarioS <mario.sguario@gmail.com>
2025-02-03 01:08:10 +0100

    fmt + clippy

--- src/utils/single_creator.rs
+++ src/utils/single_creator.rs
@@ -1,10 +1,10 @@
-
 use crate::factory::DroneRunnable;
 use wg_2024::drone::Drone;
 
 use crate::network::{DroneInfo, NodeInfo, TypeInfo};
 use crate::utils::factory::{DroneEvent, DroneFactory, NodeId, Packet};
 use crossbeam_channel::{unbounded, Sender};
+use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
 use std::thread;
 
@@ -20,7 +20,13 @@ use rustbusters_drone::RustBustersDrone;
 use wg_2024_rust::drone::RustDrone;
 use LeDron_James::Drone as LeDronJames;
 
-pub fn create_drone(id: NodeId, pdr: f32, event_send: Sender<DroneEvent>, ngbs_packet_channels: &HashMap<NodeId, Sender<Packet>>) -> NodeInfo {
+#[must_use]
+pub fn create_drone(
+    id: NodeId,
+    pdr: f32,
+    event_send: Sender<DroneEvent>,
+    ngbs_packet_channels: &HashMap<NodeId, Sender<Packet>, RandomState>,
+) -> NodeInfo {
     let drone_factories = drone_factories!(
         RustafarianDrone,
         DrOnes,
@@ -33,10 +39,7 @@ pub fn create_drone(id: NodeId, pdr: f32, event_send: Sender<DroneEvent>, ngbs_p
         RustDrone,
         RustBustersDrone
     );
-    run_drone(&drone_factories[id as usize % drone_factories.len()], id, pdr, event_send, ngbs_packet_channels)
-}
-
-fn run_drone (factory: &DroneFactory, id: NodeId, pdr: f32, event_send: Sender<DroneEvent>, ngbs_packet_channels: &HashMap<NodeId, Sender<Packet>>) -> NodeInfo {
+    let factory = &drone_factories[id as usize % drone_factories.len()];
     let (command_send, command_rcv) = unbounded();
     let (packet_in, packet_rcv) = unbounded();
 
@@ -50,13 +53,13 @@ fn run_drone (factory: &DroneFactory, id: NodeId, pdr: f32, event_send: Sender<D
     );
 
     thread::spawn(move || drone.run());
-    
+
     let type_info = TypeInfo::Drone(DroneInfo {
         pdr,
         command_send_channel: command_send,
     });
     NodeInfo {
-        neighbours: ngbs_packet_channels.keys().cloned().collect(),
+        neighbours: ngbs_packet_channels.keys().copied().collect(),
         packet_in_channel: packet_in,
         type_info,
     }
